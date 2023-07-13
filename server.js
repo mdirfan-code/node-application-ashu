0c51d8eea7a5add82cc456de33e914f5a1f389e8
mdirfan.code <mdirfan.code@gmail.com>
2023-07-14 00:29:19 +0530

    change port

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.get('/executeQuery', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log('Server is running on port 3000');
 });
