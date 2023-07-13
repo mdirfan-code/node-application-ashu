f1675690497cefc03c04df820128e43fe0b66069
mdirfan.code <mdirfan.code@gmail.com>
2023-07-13 23:36:58 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,60 @@
+// Import dependencies
+const express = require('express');
+const sql = require('mssql');
+
+// Create an instance of the Express server
+const app = express();
+
+// Azure SQL Database configuration
+const config = {
+  server: 'xyz-db-server.database.windows.net',
+  database: 'xyz-database',
+  user: 'xyz-db-server-user',
+  password: 'Ashu@1234',
+  options: {
+    encrypt: true // Use encryption
+  }
+};
+
+// API route to create a table
+app.get('/createTable', async (req, res) => {
+  try {
+    // Connect to the Azure SQL Database
+    await sql.connect(config);
+
+    // Create a new table
+    const result = await sql.query(`
+      CREATE TABLE TestTable (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(50),
+        Age INT
+      )
+    `);
+
+    res.send('Table created successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating table.');
+  }
+});
+
+// API route to execute a query
+app.get('/executeQuery', async (req, res) => {
+  try {
+    // Connect to the Azure SQL Database
+    await sql.connect(config);
+
+    // Execute a sample query
+    const result = await sql.query('SELECT * FROM TestTable');
+
+    res.send(result.recordset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error executing query.');
+  }
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
